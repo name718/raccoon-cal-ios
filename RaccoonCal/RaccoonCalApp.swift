226725e7a90d7c4260f7263d9e2dfd481b6d7c27
majuntao <majuntao_v@didiglobal.com>
2026-03-11 16:58:55 +0800

    Initial Commit

---
+++ RaccoonCal/RaccoonCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccoonCalApp.swift
+//  RaccoonCal
+//
+//  Created by didi on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct RaccoonCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
